8587c6a5041220d97f1f32ea1b5a0a15fe5abd7a
Gungun339 <mehragungun458@gmail.com>
2026-09-18 23:28:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from import google.generative as genai
+from import google.generativeai as genai
 
 st.set_page_config(page_title="EcoLens AI", page_icon="🌱", layout="centered")
 
